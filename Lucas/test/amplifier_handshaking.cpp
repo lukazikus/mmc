25fdc15b24f808a553c0f69e6f6486dca8e019b3
microl-laptop <jzhang.sd@gmail.com>
2018-04-29 11:19:17 -0400

    Lucas files

---
+++ Lucas/test/amplifier_handshaking.cpp
@@ -0,0 +1,275 @@
+// g++ -o test amplifier_handshaking.cpp
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/time.h>
+#include <math.h>
+
+int serialport_init (const char* serialport, int baud) {
+    struct termios toptions;
+    int fd;
+
+    fd = open(serialport, O_RDWR | O_NONBLOCK);
+    if (fd == -1) {
+        perror("serialport_init: Unable to open port");
+        return -1;
+    }
+
+    if (tcgetattr(fd, &toptions) < 0) {
+        perror("serialport_init: Couldn't get term attributes");
+        return -1;
+    }
+
+    speed_t brate = baud;
+    switch (brate) {
+        case 9600: brate = B9600; break;
+    }
+    cfsetispeed(&toptions, brate);
+    cfsetospeed(&toptions, brate);
+
+    // 8N1
+    toptions.c_cflag &= ~PARENB;
+    toptions.c_cflag &= ~CSTOPB;
+    toptions.c_cflag &= ~CSIZE;
+    toptions.c_cflag |= CS8;
+    // no flow control
+    toptions.c_cflag &= ~CRTSCTS;
+
+    //toptions.c_cflag &= ~HUPCL; // disable hang-up-on-close to avoid reset
+
+    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+
+    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    toptions.c_oflag &= ~OPOST; // make raw
+
+    // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
+    toptions.c_cc[VMIN]  = 0;
+    toptions.c_cc[VTIME] = 0;
+    //toptions.c_cc[VTIME] = 20;
+
+    tcsetattr(fd, TCSANOW, &toptions);
+    if( tcsetattr(fd, TCSAFLUSH, &toptions) < 0) {
+        perror("init_serialport: Couldn't set term attributes");
+        return -1;
+    }
+
+    return fd;
+}
+
+int serialport_flus (int fd) {
+    sleep(2);
+    return tcflush(fd, TCIOFLUSH);
+}
+
+int serialport_writebyte (int fd, uint8_t b) {
+    int n = write(fd, &b, 1);
+    if (n!=1)
+        return -1;
+    return 0;
+}
+
+void error (char * msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+int serialport_read_until (int fd, char* buf, char until, int buf_max, int timeout) {
+    char b[1];
+    int i = 0;
+    do {
+        int n = read(fd, b, 1);
+        if (n == -1) return -1;
+        if (n == 0) {
+            usleep(1*1000);
+            timeout--;
+            if (timeout == 0)   return -2;
+            continue;
+        }
+        buf[i] = b[0];
+        i++;
+    } while ( b[0] != until && i < buf_max && timeout > 0 );
+
+    buf[i] = 0;
+    return 0;
+}
+
+int main (int argc, char * argv[]) {
+    int baudrate = 9600;
+    const int buf_max = 256;
+    char eolchar = '\n';
+    char buf[buf_max];
+    int timeout = 1000;
+    int speed, speed_byte_1, speed_byte_2;
+
+    //number of amplifiers
+    int num_amp = 6;
+
+    //input power (percentage)
+    double inpow[] = {0,0,0,0,0,0};
+
+    int fd[num_amp] = {0};
+    int rc[num_amp] = {0};
+
+    /*char **optarg = (char **)malloc(sizeof(int)*num_amp);
+
+    for(int i = 0 ; i < num_amp ; i++)
+    {
+        optarg[i] = (char *)malloc(sizeof(char)*13);
+    }
+
+    sprintf(optarg[1], "/dev/ttyACM1");
+    printf("here2\n");
+    sprintf(optarg[2], "/dev/ttyACM2");
+    printf("here3\n");
+    sprintf(optarg[3], "/dev/ttyACM3");
+    printf("here4\n");
+    sprintf(optarg[4], "/dev/ttyACM4");
+    printf("here5\n");
+    sprintf(optarg[5], "/dev/ttyACM5");
+    printf("here6\n");
+    sprintf(optarg[0], "/dev/ttyACM0");
+    printf("here1\n");
+        struct timeval start;
+        double time_initial, time_current, time_last, time_elapsed, time_out;
+        gettimeofday(&start, NULL);
+        time_initial = (double) start.tv_sec+start.tv_usec*1e-6;*/
+    //initialization for the motors
+
+
+    fd[0] = serialport_init("/dev/ttyACM0", baudrate);
+    printf("amplifier: %d, fd: %d\n", 0, fd[0]);
+    rc[0] = serialport_writebyte(fd[0], (uint8_t)131);
+
+    fd[1] = serialport_init("/dev/ttyACM1", baudrate);
+    printf("amplifier: %d, fd: %d\n", 1, fd[1]);
+    rc[1] = serialport_writebyte(fd[1], (uint8_t)131);
+
+    fd[2] = serialport_init("/dev/ttyACM2", baudrate);
+    printf("amplifier: %d, fd: %d\n", 2, fd[2]);
+    rc[2] = serialport_writebyte(fd[2], (uint8_t)131);
+
+    fd[3] = serialport_init("/dev/ttyACM3", baudrate);
+    printf("amplifier: %d, fd: %d\n", 3, fd[3]);
+    rc[3] = serialport_writebyte(fd[3], (uint8_t)131);
+
+    fd[4] = serialport_init("/dev/ttyACM4", baudrate);
+    printf("amplifier: %d, fd: %d\n", 4, fd[4]);
+    rc[4] = serialport_writebyte(fd[4], (uint8_t)131);
+
+    fd[5] = serialport_init("/dev/ttyACM5", baudrate);
+    printf("amplifier: %d, fd: %d\n", 5, fd[5]);
+    rc[5] = serialport_writebyte(fd[5], (uint8_t)131);
+
+    struct timeval start;
+    double time_initial, time_current, time_last, time_elapsed, time_out;
+    gettimeofday(&start, NULL);
+    time_initial = (double) start.tv_sec+start.tv_usec*1e-6;
+
+    time_current = 0;
+
+    double freq = 10;
+
+    double input;
+
+    for(int j = 0 ; j < num_amp ; j++)
+    {
+        //fd[j] = serialport_init(optarg[j], baudrate);
+        //printf("amplifier: %d, fd: %d\n", j, fd[j]);
+        rc[j] = serialport_writebyte(fd[j], (uint8_t)131);
+    }
+
+    while(time_current < 15){
+
+        input = 0;//*sin(2*3.14159*freq*time_current)*1.1;
+
+        inpow[4] = input;
+        inpow[5] = input;
+
+    //testing with speed (voltage)
+    for(int j = 0 ; j < num_amp ; j++)
+    {
+
+        time_elapsed = time_last-time_initial;
+        if(inpow[j] > 0)
+        {
+
+            //gettimeofday(&start, NULL);
+            //double time_initial = (double) start.tv_sec+start.tv_usec*1e-6;
+
+            speed = (inpow[j]/100)*3200;
+            speed_byte_1 = speed % 32;
+            speed_byte_2 = speed / 32;
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)131);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)133);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)speed_byte_1);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)speed_byte_2);
+
+            //gettimeofday(&start, NULL);
+            //time_last = (double) start.tv_sec + start.tv_usec*1e-6;
+
+            //printf("%f", time_last-time_initial);
+        }
+        else
+        {
+            speed = (inpow[j]/-100)*3200;
+            speed_byte_1 = speed % 32;
+            speed_byte_2 = speed / 32;
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)131);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)134);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)speed_byte_1);
+            rc[j] = serialport_writebyte(fd[j], (uint8_t)speed_byte_2);
+        }
+    }
+
+    usleep(3e3);//sleep for 1 seconds
+
+    gettimeofday(&start, NULL);
+    time_current = (double) start.tv_sec+start.tv_usec*1e-6 - time_initial;
+
+}
+
+    //usleep(1e7);//sleep for 10 seconds
+
+    //stops all the coils
+    for(int j = 0 ; j < num_amp ; j++)
+    {
+        rc[j] = serialport_writebyte(fd[j], (uint8_t)224);//stop command
+        //required before you can run the motor:
+        rc[j] = serialport_writebyte(fd[j], (uint8_t)131);
+    }
+
+    //testing to recieve the temperature
+    for(int j = 0 ; j < num_amp ; j++)
+    {
+        //printf("%d\n",(sizeof(optarg)/sizeof(*optarg)));
+        printf("initialization result is %d.\n", fd[j]);
+        //serialport_flus(fd);
+        printf("before writing.\n");
+        rc[j] = serialport_writebyte(fd[j], (uint8_t)161);
+        printf("after writing.\n");
+        //serialport_flus(fd);
+        printf("before writing.\n");
+        rc[j] = serialport_writebyte(fd[j], (uint8_t)24);
+        printf("after writing.\n");
+        if (rc[j] == -1)   error("error in writing.\n");
+
+        memset(buf, 0, buf_max);
+        printf("before reading.\n");
+        rc[j] = serialport_read_until(fd[j], buf, eolchar, buf_max, timeout);
+        printf("read string:");
+        printf("%s.\n", buf);
+        for (int i = 0; i < 2; i ++) {
+        printf("  %d  ", buf[i]);
+        }
+        printf("reading result code %d.\n", rc[j]);
+    }
+}
---
+++ Undergrad_chris/vision.cpp
@@ -0,0 +1,272 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string.h>
+#include "general_header.hpp"
+#include "NETUSBCAM_API.h"
+#include "ICubeDefines.h"
+#include <unistd.h>
+#include <signal.h>
+#include "vision.h"
+
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+//Read: usable variables: Robot: r_x,r_y,r_angle; Cargo: c_x,c_y
+
+//~/Desktop/Undergraduate_Thesis/York$ g++ `pkg-config --cflags gtk+-3.0` `pkg-config --cflags opencv` -o test test.cpp `pkg-config --libs opencv` `pkg-config --libs gtk+-3.0` -lNETUSBCAM
+//g++ -o test test.cpp `pkg-config --libs gtk+-3.vision0` `pkg-config --cflags --libs opencv --libs` -lNETUSBCAM
+//~/Desktop/Undergraduate_Thesis/York$ ./test
+
+using namespace std;
+using namespace cv;
+
+
+Mat camera;
+
+bool compareContourAreas ( vector<Point> contour1, vector<Point> contour2 ) {
+    double i = fabs( contourArea(Mat(contour1)) );
+    double j = fabs( contourArea(Mat(contour2)) );
+    return ( i < j );
+}
+
+void SignalHandler(int i)
+{
+   signal(SIGINT, SignalHandler);
+}
+
+int GetImage(void *buffer, unsigned int buffersize, void *context)
+{
+  camera = Mat(1024,1280, CV_8UC3, buffer);
+}
+
+int ifframe(Mat camera){
+    Size s = camera.size();
+    int rows = s.height;
+    int cols = s.width;
+    if (rows > 0 && cols > 0){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+void* visionThread(void*) {
+		printf("@ the Beginning of visionThread().\n");
+
+		while(!killVisionThread) {
+
+        int result=0;
+        result = NETUSBCAM_Init();		// look for ICubes
+    	if(result==0){
+    		printf("No device\n");
+    		return 0; }
+
+      	signal(SIGINT, SignalHandler);		// register signal handler for Ctrl+C
+
+    	result = NETUSBCAM_Open(0);	// open camera
+    	if(result!=0){
+    		printf("Error: Open; Result = %d\n", result);
+    		return 0; }
+
+        // set the camera clock lower, if a lot of bad frames arriving
+    	result = NETUSBCAM_SetCamParameter(0,REG_PLL,20);
+    	if(result!=0){
+    		printf("Error: REG_PLL; Result = %d\n", result);
+    		return 0; }
+
+    	// if active, badframes are sent to the callback with buffersize = 0
+    	result = NETUSBCAM_SetCamParameter(0,REG_CALLBACK_BR_FRAMES,1);
+    	if(result!=0){
+    		printf("Error: REG_CALLBACK_BR_FRAMES; Result = %d\n", result);
+    		return 0; }
+
+        // set the callback to get the frame buffer
+      	result = NETUSBCAM_SetCallback(0,CALLBACK_RGB,&GetImage,NULL);
+      	if(result!=0){
+      		printf("Error: SetCallback; Result = %d\n", result);
+      		return 0; }
+
+      	 // start streaming of camera
+      	result = NETUSBCAM_Start(0);
+      	if(result!=0){
+      		printf("Error: Start; Result = %d\n", result);
+      		return 0; }
+
+
+
+      // VideoCapture cap("test2.mp4");
+      // if(!cap.isOpened()){
+      //   cout << "Error opening video stream or file" << endl;
+      //   return -1;
+      // }
+      int mouse_x_1;
+      int mouse_y_1;
+      int mouse_x_2;
+      int mouse_y_2;
+      int mouse_x_3;
+      int mouse_y_3;
+      int mouse_x_4;
+      int mouse_y_4;
+      int mouse_x_5;
+      int mouse_y_5;
+      int mouse_x_6;
+      int mouse_y_6;
+      int arena_w=400;//abs(mouse_x_1-mouse_x_2);
+      int arena_h=300;//abs(mouse_y_1-mouse_y_2);
+      int occupy[arena_h][arena_w];//the occupy matrix has the size of the arena
+      //Mat frame;
+      Mat hsv;
+      Mat mask;
+      Mat mask2;
+      Mat binaryImg;
+      while(1){
+
+        if(ifframe(camera)){
+
+            vector<vector<Point> > cnts;
+            vector<vector<Point> > cnts2;
+            //cap >> frame;
+            // if (camera.empty())
+            //   break;
+            //frame=frame(Rect(50,40,580,350)); //crop the frame
+
+            //draw arena--------------
+                    line(camera,Point(mouse_x_1,mouse_y_1),Point(mouse_x_1+arena_w,mouse_y_1),Scalar(0,255,0),3);
+                    line(camera,Point(mouse_x_1,mouse_y_1),Point(mouse_x_1,mouse_y_1-arena_h),Scalar(0,255,0),3);
+                    line(camera,Point(mouse_x_2,mouse_y_2),Point(mouse_x_1+arena_w,mouse_y_1),Scalar(0,255,0),3);
+                    line(camera,Point(mouse_x_2,mouse_y_2),Point(mouse_x_1,mouse_y_1-arena_h),Scalar(0,255,0),3);
+                    line(camera,Point(mouse_x_3,mouse_y_3),Point(mouse_x_4,mouse_y_4),Scalar(0,255,0),3);
+                    line(camera,Point(mouse_x_5,mouse_y_5),Point(mouse_x_6,mouse_y_6),Scalar(0,255,0),3);
+            //---------
+
+            cvtColor(camera,hsv,COLOR_BGR2GRAY);
+            inRange(hsv,0,80,mask);
+            Mat maskfororientation = mask;
+            erode(mask,mask,Mat(),Point(-1,-1),2);
+            dilate(mask,mask,Mat(),Point(-1,-1),2);
+        	inRange(hsv,80,230,mask2);
+            erode(mask2,mask2,Mat(),Point(-1,-1),2);
+            dilate(mask2,mask2,Mat(),Point(-1,-1),2);
+            for (int w=0; w<arena_w; w++){
+                occupy[0][w]=3;
+                occupy[arena_h-1][w]=3;
+                for (int j=mouse_y_3; j<mouse_y_4;j++){
+                    occupy[j][w]=3;
+                }
+                for (int j=mouse_y_5; j<mouse_y_6;j++){
+                    occupy[j][w]=3;
+                }
+                for (int h=0; h<arena_h; h++){
+                    occupy[h][0]=3;
+                    occupy[h][arena_w-1]=3;
+                    if (mask.at<int>(h,w)==255){
+                        occupy[h][w]=1;
+                    }
+                    if (mask2.at<int>(h,w)==255){
+                        occupy[h][w]=2;
+                    }
+                }
+            }
+
+
+            findContours(mask,cnts,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE,Point(0,0));
+        	findContours(mask2,cnts2,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE,Point(0,0));
+
+            if ((int)cnts.size()>0 && (int)cnts2.size()>0){
+
+                sort(cnts.begin(), cnts.end(),compareContourAreas);
+
+            	sort(cnts2.begin(), cnts2.end(),compareContourAreas);
+
+                //printf("size: %d\n", (int)cnts.size());
+                vector<RotatedRect> output(cnts.size());
+
+                for( int i = 0; i < cnts.size(); i++ ){
+                    output[i] = minAreaRect(cnts[i]);
+                }
+                float r_x=output[0].center.x;
+                float r_y=output[0].center.y;
+                float r_w=output[0].size.width;
+                float r_h=output[0].size.height;
+
+        		drawContours(camera,cnts,0,Scalar(0,0,255),2);
+                putText(camera,"Robot",Point((int)r_x,(int)r_y),FONT_HERSHEY_SIMPLEX,0.5,255,2);
+                Point2f center;
+                float radius;
+                //for( int i = 0; i < cnts2.size(); i++ ){
+                minEnclosingCircle(cnts2[0],center,radius);
+                //}
+                float c_x=center.x;
+                float c_y=center.y;
+                float c_r=radius;
+                circle(camera, Point((int)c_x,(int)c_y), (int)radius, Scalar(0, 255, 0), 2, 8, 0 );
+                putText(camera,"Cargo",Point((int)c_x,(int)c_y),FONT_HERSHEY_SIMPLEX,0.5,255,2);
+
+
+                //get robot angle___________________________________________________________________________
+                threshold (camera, binaryImg, 65, 255, THRESH_BINARY_INV );
+                float tempAngle = output[0].angle;          // get angle of rotated rect
+                int checkPt[8];
+                //printf("angle: %.3f, width: %.3f, height: %.3f\n", tempAngle, width, height);
+                checkPt[0] = r_x - 0.4 * r_w  * cosd(tempAngle);
+                checkPt[1] = r_y - 0.4 * r_h * sind(tempAngle);
+                checkPt[2] = r_x + 0.4 * r_h * sind(tempAngle);
+                checkPt[3] = r_y - 0.4 * r_h * cosd(tempAngle);
+                checkPt[4] = r_x + 0.4 * r_w  * cosd(tempAngle);
+                checkPt[5] = r_y + 0.4 * r_h * sind(tempAngle);
+                checkPt[6] = r_x - 0.4 * r_w  * sind(tempAngle);
+                checkPt[7] = r_y + 0.4 * r_h * cosd(tempAngle);
+
+                float r_angle;
+                int iDir = -1;
+                if (maskfororientation.at<unsigned char>(checkPt[1],checkPt[0]) == 0){
+                    iDir = 0;
+                    r_angle=output[0].angle+180;}
+                else if (maskfororientation.at<unsigned char>(checkPt[3],checkPt[2]) == 0){
+                    iDir = 1;
+                    r_angle=output[0].angle+270;}
+                else if (maskfororientation.at<unsigned char>(checkPt[5],checkPt[4]) == 0){
+                    iDir = 2;
+                    r_angle=output[0].angle+360;}
+                else if (maskfororientation.at<unsigned char>(checkPt[7],checkPt[6]) == 0){
+                    iDir = 3;
+                    r_angle=output[0].angle+90;}
+                circle(camera, Point(checkPt[iDir*2], checkPt[iDir*2+1]), 5, Scalar(255,0,0));
+                printf("Robot Angle: %.2f\n",r_angle);
+                //___________________________________________________________________________________
+                // printf("Angle: %.2f\n",r_angle);
+                // printf("X Position: %.2f\n",r_x);
+                // printf("Y Position: %.2f\n",r_y);
+
+            }
+
+
+            imshow("camera",camera);
+
+            char c=(char)waitKey(1);
+            if(c==27) break;
+          }
+      }
+
+      //pause();
+      // cap.release();
+      // destroyAllWindows();
+
+      // stop streaming of camera
+      result = NETUSBCAM_Stop(0);
+      if(result!=0){
+          printf("Error: Stop; Result = %d\n", result);
+          return 0; }
+
+      // close camera
+      result = NETUSBCAM_Close(0);
+      if(result!=0){
+          printf("Error: Close; Result = %d\n", result);
+          return 0; }
+
+      return 0;
+  }
+}
